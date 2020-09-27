536686b6620b63bb00555c39167e4b4d8e46d15d
shayzaran <selahattin_hayzaran@hotmail.com>
2020-09-27 16:59:51 +0300

    layout management

--- src/App.js
+++ src/App.js
@@ -6,20 +6,17 @@ import Starred from './pages/Starred';
 
 function App() {
   return (
-    <div>
-      <Navs />
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/starred">
-          <Starred />
-        </Route>
-        <Route>
-          <div>Not found!</div>
-        </Route>
-      </Switch>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/starred">
+        <Starred />
+      </Route>
+      <Route>
+        <div>Not found!</div>
+      </Route>
+    </Switch>
   );
 }
 
