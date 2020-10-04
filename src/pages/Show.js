1ef3db9808846dd79be94600e873f3e339460cd2
shayzaran <selahattin_hayzaran@hotmail.com>
2020-10-04 15:17:22 +0300

    create new custom hooks

--- src/pages/Show.js
+++ src/pages/Show.js
@@ -1,69 +1,16 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect, useReducer } from 'react';
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import Cast from '../components/show/Cast';
 import Details from '../components/show/Details';
 import Seasons from '../components/show/Seasons';
 import ShowMainData from '../components/show/ShowMainData';
-import { apiGet } from '../misc/config';
 import { ShowPageWrapper, InfoBlock } from './Show.styled';
+import { useShow } from '../misc/custom-hooks';
 
 const Show = () => {
   const { id } = useParams();
-  // const [show, setShow] = useState(null);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [error, setError] = useState(null);
-
-  const initialState = {
-    show: null,
-    isLoading: true,
-    error: null,
-  };
-
-  const reducer = (prevState, action) => {
-    switch (action.type) {
-      case 'FETCH_SUCCESS': {
-        return {
-          isLoading: false,
-          error: null,
-          show: action.show,
-        };
-      }
-      case 'FETCH_ERROR': {
-        return {
-          ...prevState,
-          isLoading: false,
-          error: action.error,
-        };
-      }
-      default:
-        return prevState;
-    }
-  };
-
-  const [{ show, isLoading, error }, dispatch] = useReducer(
-    reducer,
-    initialState
-  );
-
-  useEffect(() => {
-    let isMounted = true;
-    apiGet(`/shows/${id}?embed[]=seasons&embed[]=cast`)
-      .then(results => {
-        if (isMounted) {
-          dispatch({ type: 'FETCH_SUCCESS', show: results });
-        }
-      })
-      .catch(err => {
-        if (isMounted) {
-          dispatch({ type: 'FETCH_SUCCESS', error: err.message });
-        }
-      });
-    return () => {
-      isMounted = false;
-    };
-  }, [id]);
-
+  const { show, isLoading, error } = useShow(id);
   if (isLoading) {
     return <div>Data is being loaded</div>;
   }
